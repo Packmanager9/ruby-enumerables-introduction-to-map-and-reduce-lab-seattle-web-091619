b2533989627b2078aca78de45ab305858986b992
Cole Ditzler <colejditzler@gmail.com>
2019-09-13 10:27:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,3 +3,29 @@ def map_to_no_change (array)
 
   array
 end
+
+def map_to_negativize (array)
+
+  array
+end
+
+def map_to_square (array)
+a = 0
+for array[a]do
+array[a] = array[a]**2
+
+end
+
+
+  array
+end
+
+def map_to_no_change (array)
+
+  array
+end
+
+def map_to_no_change (array)
+
+  array
+end
